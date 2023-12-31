a9462c0e022c395d53164531ffbf4d424a1fc3a4
lester.delacruz <lesterdelacruz.dev@gmail.com>
2023-12-31 18:29:56 +0800

    Added database

--- index-ws.js
+++ index-ws.js
@@ -12,6 +12,15 @@ server.listen(3000, function () {
   console.log("server started on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /* Web Socket */
 
 const WebSocketServer = require("ws").Server;
@@ -28,14 +37,45 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+      VALUES (${numClients}, datetime('now')) 
+    `);
+
   ws.on("close", function close() {
     wss.broadcast(`A client has disconnected`);
     console.log("client has disconnected");
   });
 });
 
-wss.broadcast = function broadcast(data){
-  wss.clients.forEach(function each(client){
-  client.send(data)
-  })
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+/* Database */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+ `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+
+  console.log("Shutting down db");
+  db.close();
 }
